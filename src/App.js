79ea4790aa50cd2671ee9b108164b01edd889912
nhuc dang <nhucdang@nhucs-MacBook-Pro.local>
2020-10-06 06:18:21 -0400

    setup and added header

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import HomePage from "././pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/header/header.component";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
+
+//component is the homepage
+//path is the root base of the URL , in this case, localhost-this will direct to homepage
+//exact is a true or false
