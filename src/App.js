aae3ac82fc7d45b76e5144103160306caaa1414d
nhuc dang <nhucdang@nhucs-MacBook-Pro.local>
2020-10-22 17:02:56 -0400

    updated 10/22/20 with button components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HomePage from "././pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/header/header.component";
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
